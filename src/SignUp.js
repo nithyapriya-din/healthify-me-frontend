572d5e6b1125b74a85e859d783c385deb615d03d
NITHYAPRIYA M <142155664+nithyapriya-din@users.noreply.github.com>
2024-03-31 13:20:37 +0530

    Update SignUp.js

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,55 +1,275 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './SignUp.css';
+import React, { Component } from 'react';
+import 'whatwg-fetch';
 
-const SignUp = () => {
-    const navigate = useNavigate();
-    const [showPassword, setShowPassword] = useState(false);
+import {
+  getFromStorage,
+  setInStorage,
+} from '../../utils/storage';
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+class Home extends Component {
+  constructor(props) {
+    super(props);
 
-        const name = e.target.name.value;
-        const password = e.target.password.value;
+    this.state = {
+      isLoading: true,
+      token: '',
+      signUpError: '',
+      signInError: '',
+      signInEmail: '',
+      signInPassword: '',
+      signUpEmail: '',
+      signUpPassword: '',
+    };
 
-        if (name && password) {
-            // Navigate to the login page
-            navigate('/signin');
+    this.onTextboxChangeSignInEmail = this.onTextboxChangeSignInEmail.bind(this);
+    this.onTextboxChangeSignInPassword = this.onTextboxChangeSignInPassword.bind(this);
+    this.onTextboxChangeSignUpEmail = this.onTextboxChangeSignUpEmail.bind(this);
+    this.onTextboxChangeSignUpPassword = this.onTextboxChangeSignUpPassword.bind(this);
+    
+    this.onSignIn = this.onSignIn.bind(this);
+    this.onSignUp = this.onSignUp.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  componentDidMount() {
+    const obj = getFromStorage('the_main_app');
+    if (obj && obj.token) {
+      const { token } = obj;
+      // Verify token
+      fetch('/api/account/verify?token=' + token)
+        .then(res => res.json())
+        .then(json => {
+          if (json.success) {
+            this.setState({
+              token,
+              isLoading: false
+            });
+          } else {
+            this.setState({
+              isLoading: false,
+            });
+          }
+        });
+    } else {
+      this.setState({
+        isLoading: false,
+      });
+    }
+  }
+
+  onTextboxChangeSignInEmail(event) {
+    this.setState({
+      signInEmail: event.target.value,
+    });
+  }
+
+  onTextboxChangeSignInPassword(event) {
+    this.setState({
+      signInPassword: event.target.value,
+    });
+  }
+
+  onTextboxChangeSignUpEmail(event) {
+    this.setState({
+      signUpEmail: event.target.value,
+    });
+  }
+
+  onTextboxChangeSignUpPassword(event) {
+    this.setState({
+      signUpPassword: event.target.value,
+    });
+  }
+
+  onSignUp() {
+    // Grab state
+    const {
+      signUpEmail,
+      signUpPassword,
+    } = this.state;
+
+    this.setState({
+      isLoading: true,
+    });
+
+    // Post request to backend
+    fetch('/api/account/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: signUpEmail,
+        password: signUpPassword,
+      }),
+    }).then(res => res.json())
+      .then(json => {
+        console.log('json', json);
+        if (json.success) {
+          this.setState({
+            signUpError: json.message,
+            isLoading: false,
+            signUpEmail: '',
+            signUpPassword: '',
+          });
+        } else {
+          this.setState({
+            signUpError: json.message,
+            isLoading: false,
+          });
         }
-    };
+      });
+  }
 
-    const togglePasswordVisibility = () => {
-        setShowPassword(!showPassword);
-    };
+  onSignIn() {
+    // Grab state
+    const {
+      signInEmail,
+      signInPassword,
+    } = this.state;
+
+    this.setState({
+      isLoading: true,
+    });
+
+    // Post request to backend
+    fetch('/api/account/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: signInEmail,
+        password: signInPassword,
+      }),
+    }).then(res => res.json())
+      .then(json => {
+        console.log('json', json);
+        if (json.success) {
+          setInStorage('the_main_app', { token: json.token });
+          this.setState({
+            signInError: json.message,
+            isLoading: false,
+            signInPassword: '',
+            signInEmail: '',
+            token: json.token,
+          });
+        } else {
+          this.setState({
+            signInError: json.message,
+            isLoading: false,
+          });
+        }
+      });
+  }
+
+  logout() {
+    this.setState({
+      isLoading: true,
+    });
+    const obj = getFromStorage('the_main_app');
+    if (obj && obj.token) {
+      const { token } = obj;
+      // Verify token
+      fetch('/api/account/logout?token=' + token)
+        .then(res => res.json())
+        .then(json => {
+          if (json.success) {
+            this.setState({
+              token: '',
+              isLoading: false
+            });
+          } else {
+            this.setState({
+              isLoading: false,
+            });
+          }
+        });
+    } else {
+      this.setState({
+        isLoading: false,
+      });
+    }
+  }
+
+  render() {
+    const {
+      isLoading,
+      token,
+      signInError,
+      signInEmail,
+      signInPassword,
+      signUpEmail,
+      signUpPassword,
+      signUpError,
+    } = this.state;
+
+    if (isLoading) {
+      return (<div><p>Loading...</p></div>);
+    }
+
+    if (!token) {
+      return (
+        <div>
+          <div>
+            {
+              (signInError) ? (
+                <p>{signInError}</p>
+              ) : (null)
+            }
+            <p>Sign In</p>
+            <input
+              type="email"
+              placeholder="Email"
+              value={signInEmail}
+              onChange={this.onTextboxChangeSignInEmail}
+            />
+            <br />
+            <input
+              type="password"
+              placeholder="Password"
+              value={signInPassword}
+              onChange={this.onTextboxChangeSignInPassword}
+            />
+            <br />
+            <button onClick={this.onSignIn}>Sign In</button>
+          </div>
+          <br />
+          <br />
+          <div>
+            {
+              (signUpError) ? (
+                <p>{signUpError}</p>
+              ) : (null)
+            }
+            <p>Sign Up</p>
+            <input
+              type="email"
+              placeholder="Email"
+              value={signUpEmail}
+              onChange={this.onTextboxChangeSignUpEmail}
+            /><br />
+            <input
+              type="password"
+              placeholder="Password"
+              value={signUpPassword}
+              onChange={this.onTextboxChangeSignUpPassword}
+            /><br />
+            <button onClick={this.onSignUp}>Sign Up</button>
+          </div>
+
+        </div>
+      );
+    }
 
     return (
-        <div className='signup-container'>
-            <div className='signup-form-container'>
-                <h2 className="signup-heading">Sign Up</h2>
-                <form onSubmit={handleSubmit} className="signup-form">
-                    <input type="text" name="name" placeholder="Enter Your Name" className="signup-input" />
-                    <div className="password-container">
-                        <input 
-                            type={showPassword ? "text" : "password"} 
-                            name="password" 
-                            placeholder="Enter Your Password" 
-                            className="signup-input" 
-                        />
-                        {/* Button to toggle password visibility */}
-                        <button 
-                            type="button" 
-                            className="toggle-password-button"
-                            onClick={togglePasswordVisibility}
-                        >
-                            {showPassword ? 'Hide' : 'Show'}
-                        </button>
-                    </div>
-                    <button type="submit" className='signup-button'>Sign Up</button>
-                </form>
-                <p>Already have an Account? <a href='/SignIn' className='signin-link'>Sign In</a></p>
-            </div>
-        </div>
+      <div>
+        <p>Account</p>
+        <button onClick={this.logout}>Logout</button>
+      </div>
     );
-};
+  }
+}
 
-export default SignUp;
+export default Home;
+                       
